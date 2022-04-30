37ad37724075d22f92fe3aad52d9f620f1c63a97
Rabin <98199146+initialrise@users.noreply.github.com>
2022-04-30 13:56:31 +0545

    Added CSS and JS

---
+++ static/visualizer.js
@@ -0,0 +1,283 @@
+var AUDIO = AUDIO || {};
+
+AUDIO.VISUALIZER = (function () {
+    'use strict';
+
+    var INTERVAL = null;
+    var FFT_SIZE = 512;
+    var TYPE = {
+            'lounge': 'renderLounge'
+        };
+
+
+
+    function Visualizer (cfg) {
+        this.isPlaying = false;
+        this.autoplay = cfg.autoplay || false;
+        this.loop = cfg.loop || false;
+        this.audio = document.getElementById(cfg.audio) || {};
+        this.canvas = document.getElementById(cfg.canvas) || {};
+        this.canvasCtx = this.canvas.getContext('2d') || null;
+        this.author = this.audio.getAttribute('data-author') || '';
+        this.title = this.audio.getAttribute('data-title') || '';
+        this.ctx = null;
+        this.analyser = null;
+        this.sourceNode = null;
+        this.frequencyData = [];
+        this.audioSrc = null;
+        this.duration = 0;
+        this.minutes = '00';
+        this.seconds = '00';
+        this.style = cfg.style || 'lounge';
+        this.barWidth = cfg.barWidth || 2;
+        this.barHeight = cfg.barHeight || 2;
+        this.barSpacing = cfg.barSpacing || 5;
+        this.barColor = cfg.barColor || '#ffffff';
+        this.shadowBlur = cfg.shadowBlur || 10;
+        this.shadowColor = cfg.shadowColor || '#ffffff';
+        this.font = cfg.font || ['12px', 'Helvetica'];
+        this.gradient = null;
+    }
+
+    Visualizer.prototype.setContext = function () {
+        try {
+            window.AudioContext = window.AudioContext || window.webkitAudioContext;
+            this.ctx = new window.AudioContext();
+            return this;
+        } catch (e) {
+            console.info('Web Audio API is not supported.', e);
+        }
+    };
+
+
+    Visualizer.prototype.setAnalyser = function () {
+        this.analyser = this.ctx.createAnalyser();
+        this.analyser.smoothingTimeConstant = 0.6;
+        this.analyser.fftSize = FFT_SIZE;
+        return this;
+    };
+
+
+    Visualizer.prototype.setFrequencyData = function () {
+        this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
+        return this;
+    };
+
+
+    Visualizer.prototype.setBufferSourceNode = function () {
+        this.sourceNode = this.ctx.createBufferSource();
+        this.sourceNode.loop = this.loop;
+        this.sourceNode.connect(this.analyser);
+        this.sourceNode.connect(this.ctx.destination);
+
+        this.sourceNode.onended = function () {
+            clearInterval(INTERVAL);
+            this.sourceNode.disconnect();
+            this.resetTimer();
+            this.isPlaying = false;
+            this.sourceNode = this.ctx.createBufferSource();
+        }.bind(this);
+
+        return this;
+    };
+
+    Visualizer.prototype.setMediaSource = function () {
+        this.audioSrc = this.audio.getAttribute('src');
+        return this;
+    };
+
+
+    Visualizer.prototype.setCanvasStyles = function () {
+        this.gradient = this.canvasCtx.createLinearGradient(0, 0, 0, 300);
+        this.gradient.addColorStop(1, this.barColor);
+        this.canvasCtx.fillStyle = this.gradient;
+        this.canvasCtx.shadowBlur = this.shadowBlur;
+        this.canvasCtx.shadowColor = this.shadowColor;
+        this.canvasCtx.font = this.font.join(' ');
+        this.canvasCtx.textAlign = 'center';
+        return this;
+    };
+
+
+    Visualizer.prototype.loadSound = function () {
+        var req = new XMLHttpRequest();
+        req.open('GET', this.audioSrc, true);
+        req.responseType = 'arraybuffer';
+        this.canvasCtx.fillText('Loading...', this.canvas.width / 2 + 10, this.canvas.height / 2);
+
+        req.onload = function () {
+            this.ctx.decodeAudioData(req.response, this.playSound.bind(this), this.onError.bind(this));
+        }.bind(this);
+
+        req.send();
+    };
+
+ 
+    Visualizer.prototype.playSound = function (buffer) {
+        this.isPlaying = true;
+
+        if (this.ctx.state === 'suspended') {
+            return this.ctx.resume();
+        }
+
+        this.sourceNode.buffer = buffer;
+        this.sourceNode.start(0);
+        this.resetTimer();
+        this.startTimer();
+        this.renderFrame();
+    };
+
+  
+    Visualizer.prototype.pauseSound = function () {
+        this.ctx.suspend();
+        this.isPlaying = false;
+    };
+
+   
+    Visualizer.prototype.startTimer = function () {
+        var _this = this;
+        INTERVAL = setInterval(function () {
+            if (_this.isPlaying) {
+                var now = new Date(_this.duration);
+                var min = now.getHours();
+                var sec = now.getMinutes();
+                _this.minutes = (min < 10) ? '0' + min : min;
+                _this.seconds = (sec < 10) ? '0' + sec : sec;
+                _this.duration = now.setMinutes(sec + 1);
+            }
+        }, 1000);
+    };
+
+ 
+    Visualizer.prototype.resetTimer = function () {
+        var time =  new Date(0, 0);
+        this.duration = time.getTime();
+    };
+
+ 
+    Visualizer.prototype.onError = function (e) {
+        console.info('Error decoding audio file. -- ', e);
+    };
+
+    Visualizer.prototype.renderFrame = function () {
+        requestAnimationFrame(this.renderFrame.bind(this));
+        this.analyser.getByteFrequencyData(this.frequencyData);
+
+        this.canvasCtx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.renderTime();
+        this.renderText();
+        this.renderByStyleType();
+    };
+
+ 
+    Visualizer.prototype.renderText = function () {
+        var cx = this.canvas.width / 2;
+        var cy = this.canvas.height / 2;
+        var correction = 10;
+
+        this.canvasCtx.textBaseline = 'top';
+        this.canvasCtx.fillText('by ' + this.author, cx + correction, cy);
+        this.canvasCtx.font = parseInt(this.font[0], 10) + 8 + 'px ' + this.font[1];
+        this.canvasCtx.textBaseline = 'bottom';
+        this.canvasCtx.fillText(this.title, cx + correction, cy);
+        this.canvasCtx.font = this.font.join(' ');
+    };
+
+ 
+    Visualizer.prototype.renderTime = function () {
+        var time = this.minutes + ':' + this.seconds;
+        this.canvasCtx.fillText(time, this.canvas.width / 2 + 10, this.canvas.height / 2 + 40);
+    };
+
+
+    Visualizer.prototype.renderByStyleType = function () {
+        return this[TYPE[this.style]]();
+    };
+
+
+    Visualizer.prototype.renderLounge = function () {
+        var cx = this.canvas.width / 2;
+        var cy = this.canvas.height / 2;
+        var radius = 140;
+        var maxBarNum = Math.floor((radius * 2 * Math.PI) / (this.barWidth + this.barSpacing));
+        var slicedPercent = Math.floor((maxBarNum * 25) / 100);
+        var barNum = maxBarNum - slicedPercent;
+        var freqJump = Math.floor(this.frequencyData.length / maxBarNum);
+
+        for (var i = 0; i < barNum; i++) {
+            var amplitude = this.frequencyData[i * freqJump];
+            var alfa = (i * 2 * Math.PI ) / maxBarNum;
+            var beta = (3 * 45 - this.barWidth) * Math.PI / 180;
+            var x = 0;
+            var y = radius - (amplitude / 12 - this.barHeight);
+            var w = this.barWidth;
+            var h = amplitude / 6 + this.barHeight;
+
+            this.canvasCtx.save();
+            this.canvasCtx.translate(cx + this.barSpacing, cy + this.barSpacing);
+            this.canvasCtx.rotate(alfa - beta);
+            this.canvasCtx.fillRect(x, y, w, h);
+            this.canvasCtx.restore();
+        }
+    };
+
+  
+    function _createVisualizer (cfg) {
+        var visualizer = new Visualizer(cfg);
+
+        return function () {
+            visualizer
+                .setContext()
+                .setAnalyser()
+                .setFrequencyData()
+                .setBufferSourceNode()
+                .setMediaSource()
+                .setCanvasStyles();
+
+            return visualizer;
+        };
+    }
+
+
+    function init (cfg) {
+        return _createVisualizer(cfg)();
+    }
+
+  
+    return {
+        init: init
+    };
+})();
+
+document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+    var visualizer = AUDIO.VISUALIZER.init({
+        autoplay: true,
+        loop: true,
+        audio: 'myAudio',
+        canvas: 'myCanvas',
+        style: 'lounge',
+        barWidth: 2,
+        barHeight: 2,
+        barSpacing: 7,
+        barColor: '#cafdff',
+        shadowBlur: 20,
+        shadowColor: '#ffffff',
+        font: ['12px', 'Helvetica']
+    });
+
+    document.addEventListener('click', function (e) {
+        e.stopPropagation();
+        if (!visualizer.isPlaying) {
+            return (visualizer.ctx.state === 'suspended') ? visualizer.playSound() : visualizer.loadSound();
+        } else {
+            return visualizer.pauseSound();
+        }
+    });
+
+    if (visualizer.autoplay) {
+        visualizer.loadSound();
+    }
+}, false);
