525b6fae8a501d3702aa26b8cb4a3be8cf466cbe
Rabin <98199146+initialrise@users.noreply.github.com>
2022-04-30 18:08:16 +0545

    Added setup file

---
+++ setup.sh
@@ -0,0 +1,5 @@
+apt install opencv-python
+mkdir yolofiles
+cd yolofiles/
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+wget https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4.cfg
